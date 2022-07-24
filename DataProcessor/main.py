de3dff9063ce2499aa1f1580c5c4201dfa920e7d
barkinvar <varbarkn@gmail.com>
2022-07-24 21:59:34 +0300

    add gitignore and dataprocessor

---
+++ DataProcessor/main.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode, ZBarSymbol
+import json
+import os
+import playsound
+import time
+
+def sucessAlert():
+    playsound.playsound(path + f'\\resources\\sucess.wav', False)
+
+def qrToJson(myData):
+    DataStream = myData.split(";")
+    FinalDataStream = [i.split("=") for i in DataStream]
+    final = {sub[0]:sub[1] for sub in FinalDataStream}
+    f = open(path + f"\\ScoutDatas\\{final['l']}\\{final['l']}_{final['m']}_{final['t']}.json", "w")
+    json.dump(final, f,indent=4)
+    f.close()
+
+def videoCaptureSetCamera(cap, height, width):
+    cap.set(4,height)
+    cap.set(3,width)
+
+cap = cv2.VideoCapture(0)
+videoCaptureSetCamera(cap,640,480)
+
+path = os.getcwd()
+readQR = True
+myData = ''
+
+while readQR:
+    _, img = cap.read()
+    decoded = decode(img, symbols=[ZBarSymbol.QRCODE])   
+
+    if decoded and len(decoded) == 1:
+        color = (0,255,0)
+        barcode = decoded[0]
+        myOldData = myData
+        myData = barcode.data.decode('utf-8')
+        pts = np.array([barcode.polygon],np.int32)
+        pts2 = barcode.rect
+
+        if myOldData != myData and myData[0:2] == "s=":
+            qrToJson(myData)
+            sucessAlert()
+            color = (0,0,255)
+        
+        cv2.polylines(img,[pts],True,color,5)
+    
+    img = cv2.flip(img,1)
+    cv2.imshow('Sneaky Scanner',img)
+
+    if cv2.waitKey(30) == ord("q"):
+        readQR = False
+
