fbffbd6d126b713efdbaf9a875cbac141473f8cc
Talha Karasu <talha@karasu.xyz>
2022-07-28 10:35:10 +0300

    init pwa

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -3,6 +3,16 @@
 // The guts of the ScountingPASS application
 // Written by Team 2451 - PWNAGE
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
+
 document.addEventListener("touchstart", startTouch, false);
 document.addEventListener("touchend", moveTouch, false);
 
---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const staticSneakyScout = "sneaky-scout-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/resources/css/scoutingPASS.css",
+  "/resources/js/easy.qrcode.min.js",
+  "/resources/js/TBAInterface.js",
+  "/resources/js/scoutingPASS.js",
+  "/resources/fonts/robofan.ttf",
+  "/resources/fonts/robofan.woff",
+  "/resources/fonts/alexisv3.ttf",
+  "/resources/fonts/alex.woff",
+  "/2022/field_image.png",
+  "/2022/RR_config.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticSneakyScout).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
